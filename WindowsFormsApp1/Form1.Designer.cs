8590a07f4a5b7ce4741ab2c410ffb01e426d69b8
Kateryna Shevchuk <shevchuk.kateryna@chnu.edu.ua>
2025-05-19 15:28:33 +0300

    complite all

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -9,6 +9,12 @@ namespace MilitaryActionsGUI
         private System.Windows.Forms.Button btnAttackB;
         private System.Windows.Forms.TextBox txtLog;
 
+        // Нові контролі:
+        private System.Windows.Forms.ProgressBar progressBarA;
+        private System.Windows.Forms.ProgressBar progressBarB;
+        private System.Windows.Forms.Label lblHealthA;
+        private System.Windows.Forms.Label lblHealthB;
+
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -23,9 +29,15 @@ namespace MilitaryActionsGUI
             this.btnAttackA = new System.Windows.Forms.Button();
             this.btnAttackB = new System.Windows.Forms.Button();
             this.txtLog = new System.Windows.Forms.TextBox();
+            this.progressBarA = new System.Windows.Forms.ProgressBar();
+            this.progressBarB = new System.Windows.Forms.ProgressBar();
+            this.lblHealthA = new System.Windows.Forms.Label();
+            this.lblHealthB = new System.Windows.Forms.Label();
             this.SuspendLayout();
-
+            // 
             // btnAttackA
+            // 
+            this.btnAttackA.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
             this.btnAttackA.Location = new System.Drawing.Point(30, 20);
             this.btnAttackA.Name = "btnAttackA";
             this.btnAttackA.Size = new System.Drawing.Size(150, 40);
@@ -33,9 +45,10 @@ namespace MilitaryActionsGUI
             this.btnAttackA.Text = "Воїн A";
             this.btnAttackA.UseVisualStyleBackColor = true;
             this.btnAttackA.Click += new System.EventHandler(this.btnAttackA_Click);
-            btnAttackA.Font = new Font("Consolas", 12, FontStyle.Bold);
-
+            // 
             // btnAttackB
+            // 
+            this.btnAttackB.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
             this.btnAttackB.Location = new System.Drawing.Point(200, 20);
             this.btnAttackB.Name = "btnAttackB";
             this.btnAttackB.Size = new System.Drawing.Size(150, 40);
@@ -43,20 +56,60 @@ namespace MilitaryActionsGUI
             this.btnAttackB.Text = "Воїн B";
             this.btnAttackB.UseVisualStyleBackColor = true;
             this.btnAttackB.Click += new System.EventHandler(this.btnAttackB_Click);
-            btnAttackB.Font = new Font("Consolas", 12, FontStyle.Bold);
-
+            // 
             // txtLog
-            this.txtLog.Location = new System.Drawing.Point(30, 80);
+            // 
+            this.txtLog.Location = new System.Drawing.Point(30, 130);
             this.txtLog.Multiline = true;
             this.txtLog.Name = "txtLog";
             this.txtLog.ReadOnly = true;
             this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtLog.Size = new System.Drawing.Size(320, 200);
+            this.txtLog.Size = new System.Drawing.Size(484, 380);
             this.txtLog.TabIndex = 2;
-
+            // 
+            // progressBarA
+            // 
+            this.progressBarA.Location = new System.Drawing.Point(30, 70);
+            this.progressBarA.Name = "progressBarA";
+            this.progressBarA.Size = new System.Drawing.Size(150, 25);
+            this.progressBarA.TabIndex = 3;
+            this.progressBarA.Value = 100;
+            // 
+            // progressBarB
+            // 
+            this.progressBarB.Location = new System.Drawing.Point(200, 70);
+            this.progressBarB.Name = "progressBarB";
+            this.progressBarB.Size = new System.Drawing.Size(150, 25);
+            this.progressBarB.TabIndex = 4;
+            this.progressBarB.Value = 100;
+            // 
+            // lblHealthA
+            // 
+            this.lblHealthA.AutoSize = true;
+            this.lblHealthA.Font = new System.Drawing.Font("Consolas", 10F);
+            this.lblHealthA.Location = new System.Drawing.Point(30, 100);
+            this.lblHealthA.Name = "lblHealthA";
+            this.lblHealthA.Size = new System.Drawing.Size(314, 32);
+            this.lblHealthA.TabIndex = 5;
+            this.lblHealthA.Text = "Воїн A Здоров\'я: 100";
+            // 
+            // lblHealthB
+            // 
+            this.lblHealthB.AutoSize = true;
+            this.lblHealthB.Font = new System.Drawing.Font("Consolas", 10F);
+            this.lblHealthB.Location = new System.Drawing.Point(200, 100);
+            this.lblHealthB.Name = "lblHealthB";
+            this.lblHealthB.Size = new System.Drawing.Size(314, 32);
+            this.lblHealthB.TabIndex = 6;
+            this.lblHealthB.Text = "Воїн B Здоров\'я: 100";
+            // 
             // Form1
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.ClientSize = new System.Drawing.Size(384, 311);
+            // 
+            this.ClientSize = new System.Drawing.Size(535, 532);
+            this.Controls.Add(this.lblHealthA);
+            this.Controls.Add(this.lblHealthB);
+            this.Controls.Add(this.progressBarA);
+            this.Controls.Add(this.progressBarB);
             this.Controls.Add(this.txtLog);
             this.Controls.Add(this.btnAttackB);
             this.Controls.Add(this.btnAttackA);
@@ -64,6 +117,7 @@ namespace MilitaryActionsGUI
             this.Text = "Військова симуляція";
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
     }
 }
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace MilitaryActionsGUI
 {
@@ -20,24 +21,40 @@ namespace MilitaryActionsGUI
 
             warriorA.CounterAttack += warriorB.OnEnemyCounterAttack;
             warriorB.CounterAttack += warriorA.OnEnemyCounterAttack;
+
+            UpdateHealthBars();
         }
 
         private int attackCounter = 0;
 
         private void btnAttackA_Click(object sender, EventArgs e)
         {
+            if (warriorA.IsDead || warriorB.IsDead)
+            {
+                LogMessage("Бій завершено, один з воїнів вже повалений.");
+                return;
+            }
+
             attackCounter++;
-            LogMessage($"=== Атака №{attackCounter }===");
+            LogMessage($"=== Атака №{attackCounter} ===");
             warriorA.StartAttack();
-            LogMessage(""); 
+            LogMessage("");
+            UpdateHealthBars();
         }
 
         private void btnAttackB_Click(object sender, EventArgs e)
         {
+            if (warriorA.IsDead || warriorB.IsDead)
+            {
+                LogMessage("Бій завершено, один з воїнів вже повалений.");
+                return;
+            }
+
             attackCounter++;
             LogMessage($"=== Атака №{attackCounter} ===");
             warriorB.StartAttack();
-            LogMessage(""); 
+            LogMessage("");
+            UpdateHealthBars();
         }
 
 
@@ -45,15 +62,29 @@ namespace MilitaryActionsGUI
         {
             txtLog.AppendText(message + Environment.NewLine);
         }
+
+        private void UpdateHealthBars()
+        {
+            progressBarA.Value = warriorA.Health > 0 ? warriorA.Health : 0;
+            progressBarB.Value = warriorB.Health > 0 ? warriorB.Health : 0;
+
+            lblHealthA.Text = $"{warriorA.Name} Здоров'я: {progressBarA.Value}";
+            lblHealthB.Text = $"{warriorB.Name} Здоров'я: {progressBarB.Value}";
+        }
     }
 
     public delegate void AttackEventHandler(Warrior sender);
 
     public class Warrior
     {
-        public string Name { get; }
+        private static Random rng = new Random();
 
+        public string Name { get; }
         private readonly Action<string> logger;
+
+        public int Health { get; private set; } = 100;
+        private int counterAttackLimit = 1;
+        public bool IsDead => Health <= 0;
 
         public Warrior(string name, Action<string> logCallback)
         {
@@ -66,25 +97,101 @@ namespace MilitaryActionsGUI
 
         public void StartAttack()
         {
+            if (IsDead)
+            {
+                logger($"{Name} вже повалений і не може атакувати.");
+                return;
+            }
+
             logger($"{Name} починає атаку!");
             Attack?.Invoke(this);
         }
 
         public void StartCounterAttack()
         {
+            if (IsDead)
+            {
+                logger($"{Name} вже повалений і не може контратакувати.");
+                return;
+            }
+
+            if (counterAttackLimit <= 0)
+            {
+                logger($"{Name} не може контратакувати більше одного разу в одній атаці.");
+                return;
+            }
+
+            counterAttackLimit--;
             logger($"{Name} виконує контратаку!");
             CounterAttack?.Invoke(this);
+            counterAttackLimit = 1;
+        }
+
+        private int CalculateDamage(int minDamage, int maxDamage)
+        {
+            int damage = rng.Next(minDamage, maxDamage + 1);
+
+            // Критичний удар 15%
+            bool isCritical = rng.Next(100) < 15;
+            if (isCritical)
+            {
+                damage *= 2;
+                logger($"{Name} наносить критичний удар! Урон подвоєний.");
+            }
+
+            return damage;
+        }
+
+        private bool TryBlock()
+        {
+            // Блок 20%
+            bool blocked = rng.Next(100) < 20;
+            if (blocked)
+            {
+                logger($"{Name} блокує частину урону!");
+            }
+            return blocked;
         }
 
         public void OnEnemyAttack(Warrior enemy)
         {
-            logger($"{Name} реагує на атаку від {enemy.Name}.");
+            if (IsDead) return;
+
+            int baseDamage = CalculateDamage(8, 15);
+
+            // Перевірка блокування
+            bool blocked = TryBlock();
+            int finalDamage = blocked ? baseDamage / 2 : baseDamage;
+
+            Health -= finalDamage;
+
+            logger($"{Name} отримує атаку від {enemy.Name} на {finalDamage} урону. Здоров'я: {Health}");
+
+            if (IsDead)
+            {
+                logger($"{Name} повалений у бою!");
+                return;
+            }
+
             StartCounterAttack();
         }
 
         public void OnEnemyCounterAttack(Warrior enemy)
         {
-            logger($"{Name} отримав контратаку від {enemy.Name}.");
+            if (IsDead) return;
+
+            int baseDamage = CalculateDamage(4, 8);
+            bool blocked = TryBlock();
+            int finalDamage = blocked ? baseDamage / 2 : baseDamage;
+
+            Health -= finalDamage;
+
+            logger($"{Name} отримує контратаку від {enemy.Name} на {finalDamage} урону. Здоров'я: {Health}");
+
+            if (IsDead)
+            {
+                logger($"{Name} повалений у бою!");
+            }
         }
     }
 }
